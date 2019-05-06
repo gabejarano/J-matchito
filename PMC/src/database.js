66d536bac34bd16c08bd38c0a1ba5561588f0cf0
Gustavo Bejarano <ga.bejarano10@uniandes.edu.co>
2019-05-06 11:27:45 -0500

    terminado ver personalidad

--- PMC/src/database.js
+++ PMC/src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/jmatchito-final';
+const URI = 'mongodb://localhost/jmatchito-ahorasi';
 
 mongoose.connect(URI)
         .then(db => console.log('DB is connected'))
